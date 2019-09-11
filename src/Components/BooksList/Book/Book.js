aa8dfdefe54039ac8d10f9b9486f33b65db3c1c9
PawelSzy <peal1982@gmail.com>
2019-09-11 22:06:22 +0200

    Add check to read, add font avesome

--- src/Components/BooksList/Book/Book.js
+++ src/Components/BooksList/Book/Book.js
@@ -3,36 +3,61 @@ import './Book.scss'
 import { Button, ButtonToolbar } from 'react-bootstrap';
 import { connect } from 'react-redux';
 import * as actions from '../../../State/book';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-const book = (props) => (
-  <div className="book row border-top border-bottom">
-    <div className="col-md-2 my-1">
-      <img className="book__image img-fluid" src={props.image} alt="book cover {props.title}" />
-    </div>
+import { library } from '@fortawesome/fontawesome-svg-core'
+//import { fab } from '@fortawesome/free-brands-svg-icons'
+import { faCheck } from '@fortawesome/free-solid-svg-icons'
 
-    <div className="col-md-6 my-auto text-md-left">
-      <div className="book__title">{props.title}</div>
-      <div className="book__author">{props.author}</div>
-      <div className="book__rating">rating: {props.rating}</div>
-      <div className="row col-md-4 col-xl-2 pl-0 book__smaller-text mx-auto mx-md-0">
-        <div className="book__price col-md-6 px-0">price: {props.price}</div>
-        <div className="book__score col-md-6">score: {props.score}</div>
+library.add(faCheck)
+
+const book = (props) => {
+  const isBookMarkedToRead = props.readedBooks.includes(props.id)
+  return (
+    <div className="book row border-top border-bottom">
+      <div className="col-md-2 my-1">
+        <img className="book__image img-fluid" src={props.image} alt="book cover {props.title}" />
+      </div>
+
+      <div className="col-md-6 my-auto text-md-left">
+        <div className="book__title">{props.title}</div>
+        <div className="book__author">{props.author}</div>
+        <div className="book__rating">rating: {props.rating}</div>
+        <div className="row col-md-4 col-xl-2 pl-0 book__smaller-text mx-auto mx-md-0">
+          <div className="book__price col-md-6 px-0">price: {props.price}</div>
+          <div className="book__score col-md-6">score: {props.score}</div>
+        </div>
+      </div>
+
+      <div className="col-md-4 my-auto">
+      {
+        (!isBookMarkedToRead)
+          ?
+            <Button variant="primary" size="sm" onClick={() => props.readLater(props.id)}>
+              Want to read
+            </Button>
+          :
+          <Button variant="success" size="sm" onClick={() => props.deleteFromReadLater(props.id)}>
+            <FontAwesomeIcon icon={faCheck} /> Want to read
+          </Button>
+        }
       </div>
     </div>
+  )
+}
 
-    <div className="col-md-4 my-auto">
-      <Button variant="primary" size="sm" onClick={() => props.readLater(props.id)}>
-       Want to read
-      </Button>
-    </div>
-  </div>
-)
+const mapStateToProps = state => {
+  return {
+    readedBooks: state.book.wantToRead
+  }
+}
 
 const mapDispatchToProps = dispatch => {
   return {
-    readLater: (bookId) => dispatch( actions.readLaterBook(bookId))
+    readLater: (bookId) => dispatch( actions.readLaterBook(bookId)),
+    deleteFromReadLater: bookId => dispatch(actions.removeReadLaterBook(bookId))
   };
 };
 
 
-export default connect(null, mapDispatchToProps)(book)
+export default connect(mapStateToProps, mapDispatchToProps)(book)
--- src/State/book.js
+++ src/State/book.js
@@ -1,12 +1,16 @@
 // ACTION TYPES (there may be more than one)
 const LOAD_BOOKS = 'book/LOAD_BOOKS'
 const WANT_TO_READ = 'book/WANT_TO_READ'
+const REMOVE_WANT_TO_READ = 'book/REMOVE_WANT_TO_READ'
 
 // ACTION CREATOR - in this file it is a THUNK
 export const readLaterBook = (bookId) => {
   return { type: WANT_TO_READ, bookId }
 }
 
+export const removeReadLaterBook = (bookId) => {
+  return { type: REMOVE_WANT_TO_READ, bookId }
+}
 
 // INITIAL VALUE
 const initialState = {
@@ -19,9 +23,17 @@ export default (state = initialState, action = {}) => {
   switch (action.type) {
     // Number of cases should be equal to number of action types
     case WANT_TO_READ:
+      if (state.wantToRead.includes(action.bookId)) {
+        return state;
+      }
       return {
         ...state,
       wantToRead: state.wantToRead.concat([action.bookId])
+      }
+    case REMOVE_WANT_TO_READ:
+      return {
+        ...state,
+        wantToRead: state.wantToRead.filter(bookId => bookId !== action.bookId)
       }
     default:
       return state
