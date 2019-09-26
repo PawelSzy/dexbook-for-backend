fbaaa015e54d7c023a4841324e2e69a9eea6e102
PawelSzy <peal1982@gmail.com>
2019-09-26 17:48:57 +0200

    Change links in dropdown

--- src/Components/NavMenu/NavMenu.js
+++ src/Components/NavMenu/NavMenu.js
@@ -15,11 +15,7 @@ const NavMenu = (props) => (
         <NavLink className="navbar__link nav-link" to={"/your-books"}>Your Books</NavLink>
         <NavDropdown title="Dropdown" id="basic-nav-dropdown">
           <NavLink className="navbar__link nav-link" to={"/libraries"}>Libraries near you</NavLink>
-          <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-          <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-          <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
           <NavDropdown.Divider />
-          <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
         </NavDropdown>
       </Nav>
 
